f7a049495a15bd748dcb479d1dd85f7411c12112
PRXStore <167324132+PRXStore@users.noreply.github.com>
2025-04-09 02:25:23 +0300

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,9 +1,9 @@
 fx_version "cerulean"
 
-description "Basic React (TypeScript) & Lua Game Scripts Boilerplate"
-author "Project Error"
+description ""
+author "PRX Store"
 version '1.0.0'
-repository 'https://github.com/project-error/fivem-react-boilerplate-lua'
+repository 'https://github.com/PRXStore/React-BoilerPlate'
 
 lua54 'yes'
 
