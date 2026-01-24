55e293e5ab1c9c8304ba1f893568d60fca9d7b08
PRXStore <167324132+PRXStore@users.noreply.github.com>
2026-01-24 16:15:58 +0300

    Refactor App component and remove unused code

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -1,58 +1,7 @@
-import React, { useState } from "react";
-import "./App.css";
-import { debugData } from "./utils/debugData";
-import { fetchNui } from "./utils/fetchNui";
-
-debugData([
-  {
-    action: "setVisible",
-    data: true,
-  },
-]);
-
-
-
-const ReturnClientDataComp = ({
-  data,
-}) => (
-  <>
-    <h5>Returned Data:</h5>
-    <pre>
-      <code>{JSON.stringify(data, null)}</code>
-    </pre>
-  </>
-);
-
-
-
-const App = () => {
-  const [clientData, setClientData] = useState(null);
-
-  const handleGetClientData = () => {
-    fetchNui("getClientData")
-      .then((retData) => {
-        console.log("Got return data from client scripts:");
-        console.dir(retData);
-        setClientData(retData);
-      })
-      .catch((e) => {
-        console.error("Setting mock data due to error", e);
-        setClientData({ x: 500, y: 300, z: 200 });
-      });
-  };
-
+export default function App() {
   return (
-    <div className="nui-wrapper">
-      <div className="popup-thing">
-        <div>
-          <h1>This is the NUI Popup!</h1>
-          <p>Exit with the escape key</p>
-          <button onClick={handleGetClientData}>Get Client Data</button>
-          {clientData && <ReturnClientDataComp data={clientData} />}
-        </div>
-      </div>
+    <div>
+      App
     </div>
-  );
-};
-
-export default App;
+  )
+}
